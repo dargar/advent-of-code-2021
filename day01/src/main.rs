b9d611f5878c4dc235f8c30607bc268415cc5693
Fredrik Estelius <fredrik.estelius@gmail.com>
2021-12-01 06:39:56 +0100

    Day 01

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../input");
+    println!("First answer: {}", first_answer(input));
+    println!("Second answer: {}", second_answer(input));
+}
+
+fn first_answer(input: &str) -> usize {
+    input.lines()
+        .map(|line| line.parse::<u32>().unwrap())
+        .collect::<Vec<_>>()
+        .windows(2)
+        .filter(|depths| depths[0] < depths[1])
+        .count()
+}
+
+fn second_answer(input: &str) -> usize {
+    input.lines()
+        .map(|line| line.parse::<u32>().unwrap())
+        .collect::<Vec<_>>()
+        .windows(3)
+        .map(|depths| depths.iter().sum())
+        .collect::<Vec<u32>>()
+        .windows(2)
+        .filter(|depths| depths[0] < depths[1])
+        .count()
 }
