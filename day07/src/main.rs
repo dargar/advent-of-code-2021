5b323c30c4e3de88b925fad990cafe843c940db9
Fredrik Estelius <fredrik.estelius@gmail.com>
2021-12-07 19:37:07 +0100

    Day 07

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,3 +1,40 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../input");
+    println!("First answer: {}", first_answer(input));
+    println!("Second answer: {}", second_answer(input));
+}
+
+fn first_answer(input: &str) -> isize {
+    let positions = input
+        .trim()
+        .split(",")
+        .map(|s| s.parse::<isize>().unwrap())
+        .collect::<Vec<isize>>();
+    let min = *positions.iter().min().unwrap();
+    let max = *positions.iter().max().unwrap();
+    (min..=max)
+        .map(|p| positions.iter().map(|c| c - p).map(|c| c.abs()).sum())
+        .min()
+        .unwrap()
+}
+
+fn second_answer(input: &str) -> isize {
+    let positions = input
+        .trim()
+        .split(",")
+        .map(|s| s.parse::<isize>().unwrap())
+        .collect::<Vec<isize>>();
+    let min = *positions.iter().min().unwrap();
+    let max = *positions.iter().max().unwrap();
+    (min..=max)
+        .map(|p|
+            positions
+                .iter()
+                .map(|c| c - p)
+                .map(|c| c.abs())
+                .map(|c| (1..=c).sum::<isize>())
+                .sum()
+        )
+        .min()
+        .unwrap()
 }
